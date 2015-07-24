30fc4abf803f7a8383fb984cd6351fcab0d326d3
ipushpull <ipushpull@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-24 09:43:55 +0000

    Initial commit
    git-svn-id: https://plugins.svn.wordpress.org/ipushpull/trunk@1205609 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ipushpull/ipushpull.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @package iPushPull_Embed_Page
+ * @version 1.0
+ */
+/*
+Plugin Name: iPushPull
+Plugin URI: http://wordpress.org/plugins/ipushpull/
+Description: iPushPull plugin
+Author: iPushPull
+Version: 1.0
+Author URI: https://www.ipushpull.com
+*/
+
+
+/**
+ * @param $atts
+ * @param null $content
+ * @return string
+ *
+ * Create ipushpull_page shortcode
+ */
+function ippEmbedMarkup($atts, $content = null) {
+    extract(shortcode_atts(array(
+        "page" => null,
+        "folder" => null,
+        "back" => false,
+        "login" => false,
+        "width" => '100%',
+        "height" => '400px',
+        "interval" => null
+    ),$atts));
+
+    if (!$page || !$folder)
+        return '';
+
+    // Process back and login
+    $back = ($back || $back=='yes')?1:0;
+    $login = ($login || $login=='yes')?1:0;
+
+    $attrs = array(
+        'id'=>'ippPage_'.$folder.'_'.$page,
+        'name'=>'ipp_page_'.$folder.'_'.$page,
+        'class' => 'ipushpull-page',
+        'style' => 'width:'.$width.';height:'.$height.';border:none;',
+        'width' => $width,
+        'height' => $height,
+        'border' => '0',
+        'src' => 'https://www.ipushpull.com/embed/'.$folder.'/'.$page.'/?back='.$back.'&login='.$login.(($interval)?'&pull_interval='.$interval:'')
+    );
+
+    $markup = '<iframe';
+
+    foreach($attrs as $key => $val){
+        $markup .= ' '.$key.'="'.$val.'"';
+    }
+
+    $markup .= '></iframe>';
+    return $markup;
+}
+
+add_shortcode('ipushpull_page','ippEmbedMarkup');
